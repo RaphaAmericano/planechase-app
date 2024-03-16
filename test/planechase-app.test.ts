d16388b0e0e4bd6999f04241e9a5667a57652eda
Raphael Americano <raphael@raphaelamericano.com.br>
2024-03-16 19:43:55 -0300

    Initial commit

---
+++ test/planechase-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PlanechaseApp from '../lib/planechase-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/planechase-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PlanechaseApp.PlanechaseAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
